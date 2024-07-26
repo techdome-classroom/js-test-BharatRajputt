b5c54830bca6fb7dbabdd3b33063a103768571d2
BharatRajputt <you@example.com>
2024-07-26 12:37:45 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,7 @@ const getTotalIsles = function (grid) {
   function  describe() {
     const result = getTotalIsles([["L","L","L","L","W"],["L","L","W","L","W"],["L","L","W","W","W"],["W","W","W","W","W"]]);
     assert.equal(result, 1);
-});
+};
  
 };
 getTotalIsles()
