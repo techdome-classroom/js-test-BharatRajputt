392e96e4cd3f816e7fab96fb4bd9a72f210b0fd5
BharatRajputt <you@example.com>
2024-07-26 13:30:05 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,6 +1,6 @@
 const decodeTheRing = function (s, p) {
- const m = s.length;
- const n = p.length;
+ const mm = s.length;
+ const nn = p.length;
 
 
  const dp = Array.from({length:m+1},()=>Array(n+1).fill(false));
